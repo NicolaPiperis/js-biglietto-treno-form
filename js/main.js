94583b0afbbb227866e8e7122870659c931242b1
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-04-28 17:53:05 +0200

    ticket part

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Declare the global variables
-let numberKm, userAge, userName, basePrice, finalPrice;
+let numberKm, userAge, userName, basePrice, finalPrice, ticket_discount, wagon, random_cp, generator, exit, ticket_card;
 
 // The program will ask for the user's name
 userName = document.getElementById("name_input").value;
@@ -16,3 +16,61 @@ console.log( "the users is " + userAge );
 // The ticket price is defined on the basis of km (€0.21 per km)
 basePrice = (numberKm * 0.21)
 console.log("The base price is :" + basePrice + "€");
+
+if (userAge === "minorenne" ) {// A 20% discount is applied for minors
+    finalPrice = basePrice - (basePrice * 20 / 100) ;
+}
+// A 40% discount is applied for over 65s.
+else if ( userAge === ">65" ) {
+    finalPrice = basePrice - (basePrice * 40 / 100);
+} 
+else {// Based on this information, he will calculate the total price of the trip
+    finalPrice = basePrice;
+}
+console.log("The final price is " + finalPrice + "€");
+
+// OUTPUT
+
+    // name
+    document.getElementById("user_name").innerHTML = userName;  
+
+    // ticket discount
+    if (userAge === "minorenne" ) {
+        ticket_discount =  "Sconto studente"
+    }
+    else if ( userAge === ">65" ) {
+        ticket_discount = "Tariffa scontata"
+    }
+    else{
+        ticket_discount = "Tariffa base"
+    }
+    console.log("L'entità della tariffa è :" + ticket_discount);
+
+    document.getElementById("ticket_discount").innerHTML = ticket_discount; 
+
+    // wagon
+    wagon = Math.floor(Math.random() * 25) + 1;
+    console.log(wagon);
+
+    document.getElementById("wagon").innerHTML = wagon; 
+
+    // random cp 
+    random_cp = Math.floor(Math.random() * 750) + 1;
+    console.log(random_cp);
+
+    document.getElementById("random_cp").innerHTML = random_cp; 
+
+    // ticket price
+    document.getElementById("finalPrice").innerHTML = finalPrice + "€"
+
+
+// generator and exit
+generator = document.getElementById("generator");
+ticket_card = document.getElementById("ticket");
+
+generator.addEventListener("click", 
+    function () {
+        ticket_card.classList.Add("active")
+    }
+    console.log();
+)
